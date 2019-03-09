9a50c200eaa714c8fb21c5c9b1858a1f85e41f2a
Linigari-Luke-216307639 <46363148+Linigari-Luke-216307639@users.noreply.github.com>
2019-03-09 12:36:16 -0800

    waiting

--- src/components/title.js
+++ src/components/title.js
@@ -3,7 +3,7 @@ function Title() {
   return (
     <div>
       <h1>Migo</h1>
-      <p>choose what type of application yoiu want to make!</p>
+      <p>Choose what type of application yoiu want to make!</p>
       <p>if you want to do another run, click about then home!</p>
     </div>
   );
